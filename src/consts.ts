161d93cba6c58450fada15359400d31a04f77ba1
RafaCMur <rafacabanillasm@gmail.com>
2025-02-12 17:32:24 +0100

    feat: add resume

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,5 @@
 
 export const GITHUB_URL = "https://github.com/RafaCMur";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/rafael-cabanillas/";
+export const RESUME_URL =
+  "https://www.canva.com/design/DAGYuSrIQVI/jXLUnCl_nhNqGOauS7w6oQ/view?utm_content=DAGYuSrIQVI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=he20595df8b";
