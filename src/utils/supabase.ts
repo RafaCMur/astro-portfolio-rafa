cc962d2cd11db014ebd13b81f685e1d0aa67d811
RafaCMur <rafacabanillasm@gmail.com>
2025-02-20 23:13:52 +0100

    chore: add supabase variables to .env file

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dgdsspedxmlufmuukizl.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnZHNzcGVkeG1sdWZtdXVraXpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5OTQ4NzIsImV4cCI6MjA1NTU3MDg3Mn0.xYF6aU_-Kt8jfKDDRGYpgU00WiQ9JhIDwyJgrX5T_NU";
-// TODO: Add process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_KEY,
+);
