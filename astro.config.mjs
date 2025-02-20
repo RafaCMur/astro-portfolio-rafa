1377418cb3198a00bf9bddef9097af70e37c1d64
RafaCMur <rafacabanillasm@gmail.com>
2025-02-20 22:56:47 +0100

    chore: add vercel SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server", // Enable API routes
+  adapter: vercel(),
   integrations: [icon()],
 });
