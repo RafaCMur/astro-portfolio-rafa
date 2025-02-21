6fefaf12cb22ca498c57cfeef1ce6e1ef76a8765
RafaCMur <rafacabanillasm@gmail.com>
2025-02-21 12:18:59 +0100

    chore: update astro.config.mjs deprecated import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
