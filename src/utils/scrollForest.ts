43bbbf886f5ca3a088b853b66be00317bb20d006
RafaCMur <rafacabanillasm@gmail.com>
2025-03-24 08:39:20 +0100

    feat: first draft

---
+++ src/utils/scrollForest.ts
@@ -0,0 +1,21 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll<HTMLElement>(".panel");
+
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".container",
+      pin: true,
+      scrub: 1,
+      end: () =>
+        "+=" +
+        (document.querySelector(".container") as HTMLElement).offsetWidth,
+    },
+  });
+});
