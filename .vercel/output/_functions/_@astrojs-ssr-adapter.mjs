6f3876fb1174fdac437c08799048fa3969e1d087
RafaCMur <rafacabanillasm@gmail.com>
2025-07-20 22:13:04 +0200

    fix: remove vercel files

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_CTO6Qf9i.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Bm6XT3uV.mjs';
-export { renderers } from '../renderers.mjs';
