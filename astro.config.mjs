7a9bf859ee1fc6754db4b0c0f6bf06d1d29bf6f2
RafaCMur <rafacabanillasm@gmail.com>
2026-03-10 18:19:17 +0100

    chore: remove email functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
-// @ts-nocheck
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel";
-
-// https://astro.build/config
 export default defineConfig({
-  output: "server", // Enable API routes
-  adapter: vercel(),
   integrations: [icon()],
 });
